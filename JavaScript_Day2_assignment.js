6536662c4928a8b3dbf397d3dc1193667eb85d3c
BagchiBoi123 <59537306+BagchiBoi123@users.noreply.github.com>
2021-07-07 16:19:51 +0530

    JavaScript ZERO TO HERO-Day2 Assignment Submitted

---
+++ JavaScript_Day2_assignment.js
@@ -0,0 +1,12 @@
+//JavaScript ZERO TO HERO - Day 2
+
+//Write a program to print 5 stars in a single line
+
+
+let result = "";
+for(let i=1;i<=5;i++)
+{
+result=result+"*"+" ";
+}
+console.log("Printing 5 stars in a single line : ")
+console.log(result)
